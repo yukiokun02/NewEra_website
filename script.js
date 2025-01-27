aa5d8b54c8b8535412cf82df53f473aa05e45f15
yukiokun02 <yukio.takahashi11@gmail.com>
2025-01-27 16:18:50 +0000

    the server files

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code to handle the interactive "Join Now" button functionality
+document.getElementById("join-btn").addEventListener("click", function() {
+    // Redirects player to Minecraft client with the server IP
+    window.location.href = "minecraft://newera.enderhost.in:25565";
+});
